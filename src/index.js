71481a7457f002265e4641773cc040446fbf6f31
Vlad <makrak.vladislav@yandex.ru>
2022-01-07 03:23:28 +0300

    feat: implemented function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+  let result;
+  let arrOnes = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  let arrDozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+ 
+  let n = number;
+  number = [...number.toString()].reverse()
+  let hunds = number[2]
+  let tens  = number[1] 
+  let units = number[0]
+
+  let ones = Number.parseInt(tens + units);
+
+  if (n == 0) {
+  	result = 'zero';
+  } else if (ones == 0) {
+    result = arrOnes[hunds]  + ' ' + 'hundred';
+  } else if (n <= 10 || n >= 10 && n <= 19) { 
+    result = arrOnes[n];
+  } else if (n >= 20 && n <= 100) {
+    result = arrDozens[tens] + ' ' + arrOnes[units];
+  } else if (ones <= 10 || ones > 10 && ones < 20) {
+    result = arrOnes[hunds]  + ' ' + 'hundred' + ' ' + arrOnes[ones];
+  } else {
+     result = arrOnes[hunds]  + ' ' + 'hundred' + ' ' + arrDozens[tens] + ' ' + arrOnes[units];
+  }
+  return result.trim();
+
 }
